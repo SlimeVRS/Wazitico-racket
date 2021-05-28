d26715e97600ca9a14649862cc2e3e971e6f94d3
Marco <marco99eu@gmail.com>
2021-05-28 13:27:12 -0600

    Added internal documentation

--- graphLogic.rkt
+++ graphLogic.rkt
@@ -2,9 +2,12 @@
 
 (provide (all-defined-out))
 
-(define globalGraph '()
-  )
-
+#|
+Add a node with no neighbours to the graph
+I: Id or name of the node and the graph
+O: The graph with the new node added 
+R: The name of the graph should be a string and the graph has to be correctly written
+|#
 (define (addNode id graph)
   (cond((null? graph)
         (list (append (list id) (list graph))))
@@ -12,9 +15,20 @@
          (append graph (list (list id '()))))     
  ))
 
+#|
+Add a connection between two nodes with the given weight
+I: Start node, end node, weight and the graph
+O: The graph with a new route 
+R: The nodes given have to be on the graph and the weight should be a number 
+|#
+
 (define (addRoutes start end weight graph)
   (addRoutes-Aux start end weight graph '())
   )
+
+#|
+Auxiliary function of addRoutes
+|#
 
 (define (addRoutes-Aux start end weight graph result)
   (cond((null? graph)
@@ -30,6 +44,12 @@
                               (append result
                                       (list(car graph)))))))))
 
+#|
+Creates a new route as a list according to the node given
+I: The name of the node and the list of the routes
+O: Node list with its connections 
+R: The node has to be defined on the graph
+|#
 (define (changeRoutes id _list)
   (changeRoutes-Aux id _list)
   )
@@ -39,19 +59,39 @@
         (list(append _list id)))
        (else
         (append _list (list id)))))
-
+#|
+Validates if a path reaches the goal node or not
+I: The goal node and the path
+O: True or false
+R: The route has to be a list
+|#
 (define (solution? end route)
   (equal? end (car route))
   )
 
+#|
+Validates if a path with routes reaches the goal node or not
+I: The goal node and the path
+O: True or false
+R: The route has to be a list
+|#
 (define (weightedSolution? end route)
   (equal? end (caar route))
   )
 
+#|
+Returns the neighbours of the node if it exist
+I: A node and the graph
+O: The neigbours of the given node
+R: The node should exists, otherwise, will return false 
+|#
 (define (neighbours node graph)
   (neighbours-Aux (assoc node graph) node graph)
   )
 
+#|
+Auxiliary function of neighbours
+|#
 (define (neighbours-Aux result node graph)
   (cond((equal? result #f)
         #f)
@@ -61,6 +101,11 @@
              (else
               (cadr result))))))
 
+#|
+Validate if a element exists on a list
+I: A node and the list
+O: True if the element exist, false otherwise
+|#
 (define (member? node _list)
   (cond((null? _list)
         #f)
@@ -69,6 +114,12 @@
        (else
         (member? node (cdr _list)))))
 
+#|
+Validate if a node with route exists on a list
+I: A node with weight and the list
+O: True if the element exist, false otherwise
+R: The node should have routes added
+|#
 (define (memberWeight? node list)
   (cond((null? list)
         #f)
@@ -77,34 +128,41 @@
        (else
         (memberWeight? node (cdr list)))))
 
-(define (connect route graph)
-  (connect-Aux route '() graph (neighbours (car route) graph))
+#|
+Gets all the neighbours of each node of the route
+I: Route and the graph
+O: A list of all the n
+R: The route should be a list
+|#
+(define (checkNeighbours route graph)
+  (checkNeighbours-Aux route '() graph (neighbours(car route) graph))
   )
 
-(define (connect-Aux route result graph neighbours)
-  (cond((null? neighbours) result)
-       (else
-        (cond((member? (car neighbours) route)
-              (connect-Aux route result graph (cdr neighbours)))
-             (else
-              (connect-Aux route (append (list (cons (car neighbours) route)) result) graph (cdr neighbours)))))))
-
-(define (connectWeight route graph)
-  (connectWeight-Aux route '() graph (neighbours(car route) graph))
-  )
-
-(define (connectWeight-Aux route result graph neighbours)
-  (cond((null? neighbours) result)
+#|
+Auxiliary function of checkNeighbours
+|#
+(define (checkNeighbours-Aux route result graph neighbours)
+  (cond((null? neighbours)
+        result)
        (else
         (cond((member? (caar neighbours) route)
-              (connectWeight-Aux route result graph (cdr neighbours)))
+              (checkNeighbours-Aux route result graph (cdr neighbours)))
              (else
-              (connectWeight-Aux route (append (list (cons (caar neighbours) route)) result) graph (cdr neighbours)))))))
+              (checkNeighbours-Aux route (append (list (cons (caar neighbours) route)) result) graph (cdr neighbours)))))))
 
+#|
+Gets all the neighbours of each node with their weight of the route
+I: Route and the graph
+O: A list of all the n
+R: The route needs to have the weight of each path
+|#
 (define (extend route graph)
   (extend-Aux route '() graph (neighbours (caar route) graph))
   )
 
+#|
+Auxiliary function of extend
+|#
 (define (extend-Aux route result graph neighbours)
   (cond((null? neighbours) result)
        (else
@@ -113,38 +171,44 @@
              (else
               (extend-Aux route (append (list (cons (car neighbours)  route)) result) graph (cdr neighbours))
               )))))
-        
-(define (invert routes _routes)
-  (cond((null? _routes)
-        routes)
+
+#|
+Invert the route given
+I: A result and a route
+O: The reversed route
+R: Result has to be empty
+|#
+(define (invert result routes)
+  (cond((null? routes)
+        result)
        (else
         (invert
-         (append (list (reverse (car _routes))) routes)
-         (cdr _routes)))))
+         (append (list (reverse (car routes))) result)
+         (cdr routes)))))
 
-(define (prof start end graph)
+#|
+Returns all the routes from a to b node
+I: Start and end node and the graph
+O: All the routes from start to the end node
+R: Graph well defined
+|#
+(define (searchAllRoutes start end graph)
   (allRoutes(list (list start)) end graph '())
   )
 
-(define (prof-aux routes end graph result)
-  (cond((null? routes)
-        (invert '() result))
-       ((solution? end (car routes))
-        (prof-aux (cdr routes)
-                  end
-                  graph
-                  (cons (car routes) result)))
-       (else
-        (prof-aux (append(connect(car routes) graph)
-                         (cdr routes))
-                  end
-                  graph
-                  result))))
-
+#|
+BFS implementation
+I: start and end node and the graph
+O: The route from start to end
+R: 
+|#
 (define (profNoWeight start end graph)
   (profNoWeight-Aux (list (list start)) end graph '())
   )
 
+#|
+Auxiliary function of profNoWeight
+|#
 (define (profNoWeight-Aux routes end graph result)
   (cond((null? routes)
         (invert '() result))
@@ -154,15 +218,24 @@
                           graph
                           (cons (car routes) result)))
        (else
-        (profNoWeight-Aux (append (connectWeight (car routes) graph) (cdr routes))
+        (profNoWeight-Aux (append (checkNeighbours (car routes) graph) (cdr routes))
                           end
                           graph
                           result))))
 
+#|
+BFS implementation but checking the weight
+I: start and end node and graph
+O: The route with each weight of the node to the other
+R: 
+|#
 (define (profWeight start end graph)
   (profWeight-Aux (list (list (list start '0))) end graph '())
   )
 
+#|
+Auxiliary function of profWeight
+|#
 (define (profWeight-Aux routes end graph result)
   (cond((null? routes)
         (invert '() result))
@@ -177,26 +250,50 @@
                         graph
                         result))))
 
-(define (distance routes)
-  (distance-Aux routes '())
+#|
+Gives the distance of the node
+I: One or more route
+O: A list with the distance
+R: 
+|#
+(define (getDistance routes)
+  (getDistance-Aux routes '())
   )
- 
-(define (distance-Aux routes resut)
+
+#|
+Auxiliary function of distance
+|#
+(define (getDistance-Aux routes resut)
   (cond((null? routes)
         resut)
        (else
-        (distance-Aux (cdr routes) (append resut (list(totalDistance 0 (car routes))))))))
+        (getDistance-Aux (cdr routes) (append resut (list(totalDistance 0 (car routes))))))))
 
-(define (totalDistance weight route)
+#|
+Gives the total distance of a route
+I: A result and the route
+O: The sum of the distances of the route
+R: Result should start at 0
+|#
+(define (totalDistance result route)
   (cond((null? route)
-        weight)
+        result)
        (else
-        (totalDistance (+ weight (cadar route)) (cdr route)))))
+        (totalDistance (+ result (cadar route)) (cdr route)))))
 
+#|
+Gives the minimun value of a route
+I: route
+O: its minimum
+R: list of numbers
+|#
 (define (minimum list)
   (minimum-Aux list (car list) 0)
   )
 
+#|
+Auxiliary function of minimum
+|#
 (define (minimum-Aux list weight index)
   (cond((null? list)
         index)
@@ -206,61 +303,59 @@
              (else
               (minimum-Aux (cdr list) (car list) (+ index 1)))))))
 
-(define (pre_shorter start end)
-  (cond
-    ((equal? start 0) (cond
-                        ((equal? end 0) (shorterRoute "a"  "a" '(("a" (("b" 7) ("c" 5) ("d" 3))) ("b" (("c" 2))) ("c" (("d" 4))) ("d" (("b" 1) ("c" 6))))))
-                        ((equal? end 1) (shorterRoute "a"  "b" '(("a" (("b" 7) ("c" 5) ("d" 3))) ("b" (("c" 2))) ("c" (("d" 4))) ("d" (("b" 1) ("c" 6))))))
-                        ((equal? end 2) (shorterRoute "a"  "c" '(("a" (("b" 7) ("c" 5) ("d" 3))) ("b" (("c" 2))) ("c" (("d" 4))) ("d" (("b" 1) ("c" 6))))))
-                        ((equal? end 3) (shorterRoute "a"  "d" '(("a" (("b" 7) ("c" 5) ("d" 3))) ("b" (("c" 2))) ("c" (("d" 4))) ("d" (("b" 1) ("c" 6))))))
-                        ))
-    ((equal? start 1) (cond
-                        ((equal? end 0) (shorterRoute "b"  "a" '(("a" (("b" 7) ("c" 5) ("d" 3))) ("b" (("c" 2))) ("c" (("d" 4))) ("d" (("b" 1) ("c" 6))))))
-                        ((equal? end 1) (shorterRoute "b"  "b" '(("a" (("b" 7) ("c" 5) ("d" 3))) ("b" (("c" 2))) ("c" (("d" 4))) ("d" (("b" 1) ("c" 6))))))
-                        ((equal? end 2) (shorterRoute "b"  "c" '(("a" (("b" 7) ("c" 5) ("d" 3))) ("b" (("c" 2))) ("c" (("d" 4))) ("d" (("b" 1) ("c" 6))))))
-                        ((equal? end 3) (shorterRoute "b"  "d" '(("a" (("b" 7) ("c" 5) ("d" 3))) ("b" (("c" 2))) ("c" (("d" 4))) ("d" (("b" 1) ("c" 6))))))
-                        ))
-    ((equal? start 2) (cond
-                        ((equal? end 0) (shorterRoute "c"  "a" '(("a" (("b" 7) ("c" 5) ("d" 3))) ("b" (("c" 2))) ("c" (("d" 4))) ("d" (("b" 1) ("c" 6))))))
-                        ((equal? end 1) (shorterRoute "c"  "b" '(("a" (("b" 7) ("c" 5) ("d" 3))) ("b" (("c" 2))) ("c" (("d" 4))) ("d" (("b" 1) ("c" 6))))))
-                        ((equal? end 2) (shorterRoute "c"  "c" '(("a" (("b" 7) ("c" 5) ("d" 3))) ("b" (("c" 2))) ("c" (("d" 4))) ("d" (("b" 1) ("c" 6))))))
-                        ((equal? end 3) (shorterRoute "c"  "d" '(("a" (("b" 7) ("c" 5) ("d" 3))) ("b" (("c" 2))) ("c" (("d" 4))) ("d" (("b" 1) ("c" 6))))))
-                        ))
-    ((equal? start 3) (cond
-                        ((equal? end 0) (shorterRoute "d"  "a" '(("a" (("b" 7) ("c" 5) ("d" 3))) ("b" (("c" 2))) ("c" (("d" 4))) ("d" (("b" 1) ("c" 6))))))
-                        ((equal? end 1) (shorterRoute "d"  "b" '(("a" (("b" 7) ("c" 5) ("d" 3))) ("b" (("c" 2))) ("c" (("d" 4))) ("d" (("b" 1) ("c" 6))))))
-                        ((equal? end 2) (shorterRoute "d"  "c" '(("a" (("b" 7) ("c" 5) ("d" 3))) ("b" (("c" 2))) ("c" (("d" 4))) ("d" (("b" 1) ("c" 6))))))
-                        ((equal? end 3) (shorterRoute "d"  "d" '(("a" (("b" 7) ("c" 5) ("d" 3))) ("b" (("c" 2))) ("c" (("d" 4))) ("d" (("b" 1) ("c" 6))))))
-                        ))
-    )
-  )
-
+#|
+Return the shortest route from node a to node b of a given graph
+I: A start and end node and a graph
+O: The shortest route
+R: 
+|#
 (define (shorterRoute start end graph)
   (shorterRoute-Aux (profWeight start end graph) (profNoWeight start end graph))
   )
 
+#|
+Auxiliary function of shorterRoute
+|#
 (define (shorterRoute-Aux routesW routes)
   (cond((null? routesW)
         routesW)
        (else
-        (shorterRoute-Aux2 (minimum (distance routesW)) routesW routes))))
+        (shorterRoute-Aux2 (minimum (getDistance routesW)) routesW routes))))
 
+#|
+Auxiliary function of shorterRoute-Aux
+|#
 (define (shorterRoute-Aux2 weight routeW routes)
   (cond((zero? weight)
         (cons (car routes) (list (totalDistance 0 (car routeW)))))
        (else
         (shorterRoute-Aux2 (- weight 1) (cdr routeW) (cdr routes)))))
 
+#|
+Returns all the routes from start to end
+I: Start and end node and the graph
+O: Routes from start to end
+R: Graph well defined
+|#
 (define (allRoutes start end graph)
-  (sorter (allRoutes-Aux (profNoWeight start end graph) (distance (profWeight start end graph)) '()) '())
+  (sorter (allRoutes-Aux (profNoWeight start end graph) (getDistance (profWeight start end graph)) '()) '())
   )
 
+#|
+Auxiliary function for allRoutes
+|#
 (define (allRoutes-Aux routes routesW result)
   (cond((null? routes)
         result)
        (else
         (allRoutes-Aux (cdr routes) (cdr routesW)  (append result (list (cons (car routes) (list (car routesW)))))))))
 
+#|
+Insert a node acorting to its value
+I: A node and a list
+O: Ordered list
+R: The list with the node added in the respective position
+|#
 (define (insert node _list)
   (cond((null? _list)
         (list node))
@@ -270,11 +365,14 @@
        (else
         (cons node _list))))
 
+#|
+Gives a list with the routes oredered from smaller to greater
+I: List with routes and a list
+O: Ordered list
+R: 
+|#
 (define (sorter routes sortedList)
   (cond((null? routes)
         sortedList)
         (else
          (sorter (cdr routes) (insert (car routes) sortedList)))))
-
-; (shorterRoute "b"  "d" '(("a" (("b" 7) ("c" 5) ("d" 3))) ("b" (("c" 2))) ("c" (("d" 4))) ("d" (("b" 1) ("c" 6)))))
-(provide (all-defined-out))
